9db71681fff67d55b86b551a82423e59432c8460
Avanguard77 <avanguard1383.w@gmail.com>
2025-11-24 16:06:15 +0330

    Add : prefab update

---
+++ Assets/_Scripts/Timber_Man/Behaviour/DamagableBehaviour/Abstraction/Basedamage.cs
@@ -0,0 +1,10 @@
+﻿using _Scripts.Timber_Man.Models.Enums;
+using UnityEngine;
+
+namespace _Scripts.Timber_Man.Behaviour.DamagableBehaviour.Abstraction
+{
+    public abstract class Basedamage : MonoBehaviour
+    {
+        public abstract void Attack();
+    }
+}
---
+++ Assets/_Scripts/Timber_Man/Behaviour/DamagableBehaviour/BranchDamage.cs
@@ -0,0 +1,13 @@
+﻿using _Scripts.Timber_Man.Behaviour.DamagableBehaviour.Abstraction;
+using _Scripts.Timber_Man.Models.Enums;
+
+namespace _Scripts.Timber_Man.Behaviour.DamagableBehaviour
+{
+    public class BranchDamage : Basedamage
+    {
+        public override void Attack()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
