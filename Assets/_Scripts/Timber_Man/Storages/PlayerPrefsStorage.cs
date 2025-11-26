9c2fe92ce5c784ce5b713983b03d160b1351bbe7
Avanguard77 <avanguard1383.w@gmail.com>
2025-11-26 20:48:48 +0330

    ref

--- Assets/_Scripts/Timber_Man/Storages/PlayerPrefsStorage.cs
+++ Assets/_Scripts/Timber_Man/Storages/PlayerPrefsStorage.cs
@@ -1,6 +1,5 @@
 ﻿using Bayegan.Builder;
 using Bayegan.Storage.Abstractions;
-using UnityEngine;
 
 namespace _Scripts.Timber_Man.Storages
 {
@@ -8,13 +7,13 @@ namespace _Scripts.Timber_Man.Storages
     {
         private readonly IBayeganDictionary _bayeganDictionaryBuilder;
 
-        private const string EncryptionKey = "12345678901234567890123456789012"; // 32 chars
-        private const string IV = "1234567890123456";
+        private const string EncryptionKey = "12345678901234567890123456789012";
+        private const string Iv = "1234567890123456";
 
         public PlayerPrefsStorage()
         {
             _bayeganDictionaryBuilder = new BayeganDictionaryBuilder()
-                .UseDefaultSecurePlayerPrefs(EncryptionKey, IV)
+                .UseDefaultSecurePlayerPrefs(EncryptionKey, Iv)
                 .Build();
         }
 
