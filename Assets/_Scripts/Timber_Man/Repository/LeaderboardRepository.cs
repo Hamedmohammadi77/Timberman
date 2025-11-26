426543124bb7845574beb4df0c3fc292e3dce54b
Avanguard77 <avanguard1383.w@gmail.com>
2025-11-26 20:44:58 +0330

    Add : bayegan library to PlayerPrefsStorage

--- Assets/_Scripts/Timber_Man/Repository/LeaderboardRepository.cs
+++ Assets/_Scripts/Timber_Man/Repository/LeaderboardRepository.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using _Scripts.Timber_Man.Domain;
 using _Scripts.Timber_Man.Extensions;
 using _Scripts.Timber_Man.Storages;
-using Newtonsoft.Json;
-using UnityEngine;
 using Zenject;
 
 
--- Assets/_Scripts/Timber_Man/Storages/PlayerPrefsStorage.cs
+++ Assets/_Scripts/Timber_Man/Storages/PlayerPrefsStorage.cs
@@ -1,24 +1,31 @@
-﻿using UnityEngine;
+﻿using Bayegan.Builder;
+using Bayegan.Storage.Abstractions;
+using UnityEngine;
 
 namespace _Scripts.Timber_Man.Storages
 {
     public class PlayerPrefsStorage
     {
+        private readonly IBayeganDictionary _bayeganDictionaryBuilder;
+
+        private const string EncryptionKey = "12345678901234567890123456789012"; // 32 chars
+        private const string IV = "1234567890123456";
+
+        public PlayerPrefsStorage()
+        {
+            _bayeganDictionaryBuilder = new BayeganDictionaryBuilder()
+                .UseDefaultSecurePlayerPrefs(EncryptionKey, IV)
+                .Build();
+        }
+
         public void Save(string key, string value)
         {
-            PlayerPrefs.SetString(key, value);
+            _bayeganDictionaryBuilder.Store(key, value);
         }
 
         public string Load(string key, string defaultValue)
         {
-            var getdata = PlayerPrefs.GetString(key);
-
-            if (!string.IsNullOrEmpty(getdata))
-            {
-                return getdata;
-            }
-
-            return defaultValue;
+            return _bayeganDictionaryBuilder.Load(key, defaultValue);
         }
     }
 }
